4c65bf029e445a8fa576a64f147dd318c2055a73
Venelin Stoykov <venelin@magicsolutions.bg>
2013-05-30 15:48:28 +0300

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='open-singly',
-    version='0.1',
+    version='0.2',
     description='Implementation of singly API for Python',
     author='Venelin Stoykov',
     author_email='venelin@magicsolutions.bg',
