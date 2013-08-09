578a26148b85dbf8eedf263a315084e84eeb8571
Venelin Stoykov <venelin@magicsolutions.bg>
2013-08-09 15:47:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+
+def get_long_description():
+    with open('./README.md', 'r') as readme:
+        return readme.read()
+
 setup(
     name='open-singly',
     version='0.2',
@@ -7,9 +12,11 @@ setup(
     author='Venelin Stoykov',
     author_email='venelin@magicsolutions.bg',
     url='https://github.com/MagicSolutions/open-singly',
+    long_description=get_long_description(),
     packages=find_packages(),
     zip_safe=True,
     install_requires=['slumber>=0.5.3'],
+    include_package_data=True,
     classifiers=[
       'Development Status :: 4 - Beta',
       'Environment :: Web Environment',
